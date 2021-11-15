6508c1ec0d2418e739ac31529b013af827f876f4
Tharuka Lakshan Dissanayake <tharukawapnet@gmail.com>
2021-11-16 03:34:04 +0530

    Added pdf generate functionality

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -8,6 +8,22 @@ import moment from "moment";
 import React, { useEffect, useState } from "react";
 import DashboardLayout from "../components/DashboardLayout";
 import { SALES_FETCH } from "../config/ApiConfig";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "column",
+    backgroundColor: "#ffffff",
+  },
+  reportHeader: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 10,
+    padding: 10,
+  },
+});
 
 const Reports = () => {
   const [value, setValue] = React.useState(0);
@@ -15,8 +31,10 @@ const Reports = () => {
     moment().subtract(1, "months").startOf("month").format("YYYY-MM-DD")
   );
   const [endDate, setEndDate] = useState(moment().format("YYYY-MM-DD"));
+  const [today, setToday] = useState(moment().format("YYYY-MM-DD"));
   const [sales, setSales] = useState([]);
   const [orderStatus, setOrderStatus] = useState("DELIVERED");
+  const [totalSale, setTotalSale] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -30,6 +48,110 @@ const Reports = () => {
     setOrderStatus(val);
   };
 
+  const MyDocument = () => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={{ border: "1px solid #000000", margin: 10 }}>
+          <View
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#cccccc",
+              padding: 10,
+            }}
+          >
+            <Text style={{ fontWeight: "bold", fontSize: "24px" }}>
+              Sales Report
+            </Text>
+          </View>
+          <View
+            style={{
+              padding: 10,
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View>
+              <Text style={{ fontSize: 14 }}>Start Date: {startDate}</Text>
+              <Text style={{ fontSize: 14 }}>End Date: {endDate}</Text>
+            </View>
+            <View>
+              <Text style={{ fontSize: 14 }}>Generated: {today}</Text>
+              <Text style={{ fontSize: 14 }}>Order Status: {orderStatus}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ margin: 10, border: "1px solid #000000" }}>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              borderBottom: "1px solid #000000",
+              padding: 10,
+            }}
+          >
+            <View>
+              <Text style={{ fontSize: 14,  width: 70 }}>Order ID</Text>
+            </View>
+            <View>
+              <Text style={{ fontSize: 14, width: 170 }}>Item Name</Text>
+            </View>
+            <View>
+              <Text style={{ fontSize: 14, width: 75 }}>Date</Text>
+            </View>
+            <View>
+              <Text style={{ fontSize: 14,  width: 75 }}>QTY</Text>
+            </View>
+            <View>
+              <Text style={{ fontSize: 14, width: 110  }}>Unit Price</Text>
+            </View>
+            <View>
+              <Text style={{ fontSize: 14 }}>Total</Text>
+            </View>
+          </View>
+
+          {sales.map((item) => {
+            return (
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  borderBottom: "1px solid #cccccc",
+                  padding: 10,
+                }}
+              >
+                <View>
+                  <Text style={{ fontSize: 11, width: 70 }}>{item.id}</Text>
+                </View>
+                <View>
+                  <Text style={{ fontSize: 11, width: 170 }}>{item.item.itemName}</Text>
+                </View>
+                <View>
+                  <Text style={{ fontSize: 11, width: 75 }}>{moment(item.orderedAt).format("YYYY-MM-DD")}</Text>
+                </View>
+                <View>
+                  <Text style={{ fontSize: 11, width: 75 }}>{item.qty}</Text>
+                </View>
+                <View>
+                  <Text style={{ fontSize: 11, width: 110 }}>{item.item.unitPrice}</Text>
+                </View>
+                <View>
+                  <Text style={{ fontSize: 11 }}>{item.item.unitPrice*item.qty}</Text>
+                </View>
+              </View>
+            );
+          })}
+          <View style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row', padding: 10}}>
+            <Text style={{ fontSize: 14, fontWeight: 'bold' }}>TOTAL:</Text>
+            <Text style={{ fontSize: 14, fontWeight: 'bold' }}>Rs {totalSale}</Text>
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+
   const fetchSales = () => {
     axios
       .get(
@@ -39,8 +161,17 @@ const Reports = () => {
       )
       .then((res) => {
         setSales(res.data.body);
+        generateTotalSale(res.data.body)
       });
   };
+
+  const generateTotalSale=(saleList)=>{
+    let tot = 0
+    saleList.forEach(item=>{
+      tot += (item.qty*item.item.unitPrice)
+    })
+    setTotalSale(tot)
+  }
 
   const salesTab = () => {
     return (
@@ -90,20 +221,25 @@ const Reports = () => {
           </div>
           <div
             class="col form-group"
-            style={{ display: "flex", justifyContent: "center" }}
+            style={{ display: "flex", justifyContent: "center", alignItems: 'center' }}
           >
             <input
               onClick={fetchSales}
               className="btn btn-sm btn-primary"
               type="button"
               value="Fetch Data"
+              style={{height: 'fit-content'}}
             />{" "}
             &nbsp;
-            <input
+            {/* <input
               className="btn btn-sm btn-success"
               type="button"
               value="Generate PDF"
-            />
+              // onClick={generatePdf}
+            /> */}
+             <PDFDownloadLink className="btn btn-sm btn-success" document={<MyDocument />} fileName="report.pdf" style={{height: 'fit-content'}}>
+              {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Generate PDF')}
+            </PDFDownloadLink>
           </div>
         </div>
         <div>
@@ -183,13 +319,13 @@ const Reports = () => {
           aria-label="disabled tabs example"
         >
           <Tab label="Sales" />
-          <Tab label="Items" />
-          <Tab label="Users" />
+          {/* <Tab label="Items" />
+          <Tab label="Users" /> */}
         </Tabs>
         <div>
           {value == 0 && salesTab()}
-          {value == 1 && itemsTab()}
-          {value == 2 && usersTab()}
+          {/* {value == 1 && itemsTab()}
+          {value == 2 && usersTab()} */}
         </div>
       </div>
     </DashboardLayout>
